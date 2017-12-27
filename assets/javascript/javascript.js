039d9762e993eb379ca430bb4fdee0154bfa0516
Pavel Gerasimenko <pavelgerasimenko@sfo514.ca.revelsystems.com>
2017-12-27 15:31:45 -0800

    html + css

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDl8CPr2FKWy-GXlrZXbOQhnQ5q-sqUGVo",
+    authDomain: "trainscheduler-17c34.firebaseapp.com",
+    databaseURL: "https://trainscheduler-17c34.firebaseio.com",
+    projectId: "trainscheduler-17c34",
+    storageBucket: "trainscheduler-17c34.appspot.com",
+    messagingSenderId: "267037135602"
+  };
+  firebase.initializeApp(config);
+</script>
